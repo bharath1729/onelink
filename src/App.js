cb94a4480964be164806922a8b59c49abfab16b3
bharath1729 <bharathreddy1729@gmail.com>
2021-10-07 23:15:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import PublicLink from './Pages/PublicLink'
+import Signin from './Pages/Signin'
+import Register from './Pages/Register'
+import Settings from './Pages/Settings'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ChakraProvider>
+       <Router>
+           <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/settings' component={Settings}/>
+            <Route exact path='/login' component={Signin}/>
+            <Route exact path='/register' component={Register}/>
+            <Route exact path='/:id' component={PublicLink}/>
+           </Switch>
+       </Router>
+       </ChakraProvider>
+    )
 }
 
-export default App;
+export default App
